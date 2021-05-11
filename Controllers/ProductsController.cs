5e8f48bcd0e8d751e1dde8d4a59991b7bf14a5b9
pradeep-1999 <51508966+pradeep-1999@users.noreply.github.com>
2021-05-11 10:05:44 +0530

    Add files via upload

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+using System.Web;
+//using System.Web.Http;
+using System.Net.Http;
+using System.Net;
+using WebApplication2.Services;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductService _productService;
+
+        private readonly EMailServices _eMailServices;
+        public ProductsController(ProductService productService, EMailServices eMailServices)
+        {
+            _productService = productService;
+            _eMailServices = eMailServices;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> Get()
+        {
+
+            // Line no 36 has been commented because i have added timer functionality in EMailServices service. 
+            // _eMailServices.fun1();
+                      
+            // EMailServices e1 = new EMailServices();
+
+            return _productService.Get();
+
+           
+        }
+
+        [HttpGet("{id:length(24)}"  , Name = "GetProduct")]
+        public ActionResult<Product> Get(string id)
+        {
+            var product = _productService.Get(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+
+            return product;
+        }
+
+        [HttpPost]
+        public ActionResult<Product> Create(Product product)
+        {
+            _productService.Create(product);
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id.ToString() }, product);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public Product Update(string id, Product productIn)
+        {
+            var product = _productService.Get(id);
+
+            if (product == null)
+            {
+                return null;
+            }
+
+
+            if (!_eMailServices.check_credentials(productIn) || !_eMailServices.IsValidEmail(productIn.Email_id))
+
+            {
+                Console.WriteLine("Check your credentials\n");
+                return null;
+            }
+          
+            
+            if (productIn.Id == null)
+                productIn.Id = id;
+
+            _productService.Update(id, productIn);
+
+            return productIn;
+        }
+
+
+        [HttpDelete("{id:length(24)}")]
+        public Product Delete(string id)
+        {
+            var product = _productService.Get(id);
+
+            if (product == null)
+            {
+                return null;
+            }
+
+            var product_deleted= _productService.Get(id);
+            
+            _productService.Remove(product.Id);
+
+            return product_deleted ;
+        }
+
+
+    }
+}
---
+++ ProductService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+using MongoDB.Driver;
+
+
+namespace WebApplication2.Services
+{
+    public class ProductService
+    {
+        public readonly IMongoCollection<Product> _products;
+
+        public ProductService(IProductDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _products = database.GetCollection<Product>(settings.ProductCollectionName);
+        
+        }
+
+        public List<Product> Get() =>
+       _products.Find(Product => true).ToList();
+
+        public Product Get(string id) =>
+            _products.Find<Product>(x => x.Id == id).FirstOrDefault();
+
+        public Product Create(Product product)
+        {
+            _products.InsertOne(product);
+            return product;
+        }
+
+       public void Update(string id, Product productIn) =>
+            _products.ReplaceOne(product => product.Id == id, productIn);                       //product => product.Id == id,
+
+
+        public void Remove(Product productIn) =>
+            _products.DeleteOne(product => product.Id == productIn.Id);
+
+        public void Remove(string id) =>
+            _products.DeleteOne(product => product.Id == id);   
+
+    }
+}
